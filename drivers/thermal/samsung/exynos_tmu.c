514c215867071d3756e5c9addffb1aa96d6ad0f5
Lukasz Majewski <l.majewski@samsung.com>
2015-01-19 12:44:03 +0100

    thermal: exynos: Reorder exynos_map_dt_data() function
    The exynos_map_dt_data() function must be called before
    thermal_zone_of_sensor_register(), and hence provide tmu_read() function,
    before it is needed.
    
    This change is driven by adding support for enabling thermal_zoneX when
    it is properly initialized.
    
    One can read the mode of operation at /sys/class/thermal/thermal_zone0/mode
    Such functionality was missing in the of-thermal.c code.
    
    Reported-by: Abhilash Kesavan <a.kesavan@samsung.com>
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -1026,6 +1026,10 @@ static int exynos_tmu_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, data);
 	mutex_init(&data->lock);
 
+	ret = exynos_map_dt_data(pdev);
+	if (ret)
+		goto err_sensor;
+
 	/*
 	 * Try enabling the regulator if found
 	 * TODO: Add regulator as an SOC feature, so that regulator enable
@@ -1043,10 +1047,6 @@ static int exynos_tmu_probe(struct platform_device *pdev)
 			return -EPROBE_DEFER;
 		dev_info(&pdev->dev, "Regulator node (vtmu) not found\n");
 	}
-
-	ret = exynos_map_dt_data(pdev);
-	if (ret)
-		goto err_sensor;
 
 	INIT_WORK(&data->irq_work, exynos_tmu_work);
 
