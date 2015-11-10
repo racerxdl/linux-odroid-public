bab7203742fdb27c8a9f873ddc1bddebc25009de
Marek Szyprowski <m.szyprowski@samsung.com>
2015-11-10 14:23:36 +0100

    drm/exynos: gem: set default alignment for dumb GEM buffers
    This patch forces all GEM buffers to have pitch aligned at least to 8
    pixels. This is a common requirement for various Exynos IPP blocks,
    which otherwise won't be able to operate on buffers of random size.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_drv.h
+++ drivers/gpu/drm/exynos/exynos_drm_drv.h
@@ -24,6 +24,8 @@
 
 #define DEFAULT_WIN	0
 
+#define EXYNOS_DRM_PITCH_ALIGN	8
+
 #define to_exynos_crtc(x)	container_of(x, struct exynos_drm_crtc, base)
 #define to_exynos_plane(x)	container_of(x, struct exynos_drm_plane, base)
 
